fc6f606b81b1eb30b01cd5ec8a6e101e7549027c
Yaroslav Mokhurenko <yaroslavmo@softwareplanet.uk.com>
2019-10-31 14:37:04 +0200

    Add getAccessList

--- src/Services/ExcelService.php
+++ src/Services/ExcelService.php
@@ -1,13 +1,9 @@
 <?php namespace DreamFactory\Core\Excel\Services;
 
-use DreamFactory\Core\Exceptions\InternalServerErrorException;
-use DreamFactory\Core\Exceptions\BadRequestException;
-use DreamFactory\Core\Exceptions\RestException;
-use DreamFactory\Core\Models\Service;
 use DreamFactory\Core\Services\BaseRestService;
 use DreamFactory\Core\Excel\Resources\SpreadsheetResource;
-use DreamFactory\Core\Utility\Session;
 use DreamFactory\Core\Enums\Verbs;
+use ServiceManager;
 
 class ExcelService extends BaseRestService
 {
@@ -19,4 +15,49 @@ class ExcelService extends BaseRestService
             'label'      => 'Spreadsheet'
         ]
     ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAccessList()
+    {
+        $list = parent::getAccessList();
+        $nameField = static::getResourceIdentifier();
+
+        foreach ($this->getResources() as $resource) {
+            $name = array_get($resource, $nameField);
+            if (!empty($this->getPermissions())) {
+                $list[] = $name . '/';
+            }
+        }
+
+        $files = array_get($this->getContainerResponse()->getContent(), 'resource', $this->getContainerResponse()->getContent());
+
+        foreach ($files as $resource) {
+            $name = array_get($resource, $nameField);
+            if (!empty($this->getPermissions())) {
+                $list[] = '_spreadsheet/' . $name . '/';
+            }
+        }
+
+        return $list;
+    }
+
+    public function getContainerResponse()
+    {
+        $serviceConfig = $this->getConfig();
+        $storageServiceId = array_get($serviceConfig, 'storage_service_id');
+        $storageContainer = array_get($serviceConfig, 'storage_container', '/');
+        $storageService = ServiceManager::getServiceById($storageServiceId);
+        $storageServiceName = $storageService->getName();
+        return ServiceManager::handleRequest(
+            $storageServiceName,
+            Verbs::GET,
+            $storageContainer,
+            [
+                'include_folders' => $this->request->getParameterAsBool('include_folders', false),
+                'as_list' => $this->request->getParameterAsBool('as_list')
+            ]
+        );
+    }
 }
