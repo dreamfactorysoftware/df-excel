61fce21f4872fb1809ec877aa571d06b7541e997
Yaroslav Mokhurenko <yaroslavmo@softwareplanet.uk.com>
2019-10-31 16:04:04 +0200

    Make handleRequest method throw other services exceptions

--- src/Services/ExcelService.php
+++ src/Services/ExcelService.php
@@ -50,7 +50,7 @@ class ExcelService extends BaseRestService
         $storageContainer = array_get($serviceConfig, 'storage_container', '/');
         $storageService = ServiceManager::getServiceById($storageServiceId);
         $storageServiceName = $storageService->getName();
-        return ServiceManager::handleRequest(
+        $response =  ServiceManager::handleRequest(
             $storageServiceName,
             Verbs::GET,
             $storageContainer,
@@ -59,5 +59,6 @@ class ExcelService extends BaseRestService
                 'as_list' => $this->request->getParameterAsBool('as_list')
             ]
         );
+        return $response;
     }
 }
